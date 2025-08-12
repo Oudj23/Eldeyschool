b6ae13d256066a486c2e27eda276c9da2ed6839b
oussamadjooo <oussamadjouider8@gmail.com>
2025-08-12 09:12:11 +0100

    Change server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 require('dotenv').config(); // Load .env variables
 
 app.use(express.json({ limit: '100mb' }));
@@ -10,7 +11,12 @@ app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'dist')));
 
+// 🔥 Toutes les routes non API => index.html (pour React Router)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 // DB connection
 require('./Config/db');
 app.get('/test', (req, res) => {
