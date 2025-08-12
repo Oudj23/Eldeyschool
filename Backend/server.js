abd7c1f5755ef23a1235c58af33b14fa3bde9671
oussamadjooo <oussamadjouider8@gmail.com>
2025-08-12 18:18:05 +0100

    Change server.js12

--- Backend/server.js
+++ Backend/server.js
@@ -1,32 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
-
 const app = express();
+require('dotenv').config(); // Load .env variables
+
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // DB connection
 require('./Config/db');
 
-// Routes
+// Admin Routes
 const adminRoutes = require('./Routes/adminRoutes');
 app.use('/api/admin', adminRoutes);
-
-// ✅ Serve frontend static files
-app.use(express.static(path.join(__dirname, '../Frontend/dist')));
-
-// ✅ Handle frontend routes (React Router)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
-});
-
-// Start serv
-const PORT = process.env.PORT;
+// Start server
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
