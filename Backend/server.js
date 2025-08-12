a764b694fc9fc5dea240806e00a50657415ffa50
oussamadjooo <oussamadjouider8@gmail.com>
2025-08-12 18:09:13 +0100

    Change server.js11

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.get('*', (req, res) => {
 });
 
 // Start serv
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
