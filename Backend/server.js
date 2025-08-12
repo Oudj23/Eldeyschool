9b57df32ec58a4d262ab8af5f9df6347d30cf423
oussamadjooo <oussamadjouider8@gmail.com>
2025-08-12 17:45:25 +0100

    Change server.js9

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'Frontend/dist')));
 
 // ✅ Handle frontend routes (React Router)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
 });
 
 // Start server
