e1b0c217a297b2203631987b0afe22ebdbb3bcb3
oussamadjooo <oussamadjouider8@gmail.com>
2025-08-13 02:29:44 +0100

    Change server.js14

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 require('dotenv').config(); // Load .env variables
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
-
+app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -17,6 +18,10 @@ require('./Config/db');
 // Admin Routes
 const adminRoutes = require('./Routes/adminRoutes');
 app.use('/api/admin', adminRoutes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
+});
+
 // Start server
 const PORT = process.env.PORT ;
 app.listen(PORT, () => {
