92d1dd3f1bf12469b96e1a948325d4fe0bec0ff4
oussamadjooo <oussamadjouider8@gmail.com>
2025-08-15 21:21:26 +0100

    Style fix css file 20

--- Frontend/src/Index/Home/Home.jsx
+++ Frontend/src/Index/Home/Home.jsx
@@ -5,7 +5,18 @@ import Why_Us from './Home_Sections/Why_Us/Why_Us';
 import Choose_Level_Section from './Home_Sections/Choose_Level_Section/Choose_Level_Section';
 import Populaire_Formation from './Home_Sections/Populaire_Formation/Populaire_Formation';
 import Footer from '../../Components/Footer/Footer.jsx';
+import { useNavigate } from 'react-router-dom'
+
 function Home() {
+    const navigate = useNavigate();
+
+    // ✅ Check if token exists on component mount
+    useEffect(() => {
+        const token = localStorage.getItem('deyschooltoken');
+        if (token) {
+            navigate('/user/dashboard');
+        }
+    }, [navigate]);
     return (
         <div className='Home-Container'>
             <Home_Hero_Section/>
