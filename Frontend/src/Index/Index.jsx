5a669b1ff572cc923c272db2265835dde21694fd
oussamadjooo <oussamadjouider8@gmail.com>
2025-08-15 21:18:34 +0100

    Style fix css file 19

--- Frontend/src/Index/Index.jsx
+++ Frontend/src/Index/Index.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Components/Header/Header';
 import './Index.css'
+import { useNavigate } from 'react-router-dom'
 function Index() {
+    const navigate = useNavigate();
+
+    // ✅ Check if token exists on component mount
+    useEffect(() => {
+        const token = localStorage.getItem('deyschooltoken');
+        if (token) {
+            navigate('/user/dashboard');
+        }
+    }, [navigate]);
     return (
         <div className='Index'>
             <Header/>
